7f151c496f28b9dbcdd9d2abb7f53c6accc9feb4
Max Milton <max@wearegenki.com>
2021-05-10 18:16:02 +1000

    Add test for Console

---
+++ test/Console.test.ts
@@ -0,0 +1,37 @@
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+// import { Console } from '../src/components/Console';
+import {
+  cleanup,
+  mocksSetup,
+  mocksTeardown,
+  render,
+  setup,
+  teardown,
+} from './utils';
+
+type ConsoleComponent = typeof import('../src/components/Console');
+
+test.before(setup);
+test.before(mocksSetup);
+test.after(mocksTeardown);
+test.after(teardown);
+test.after.each(cleanup);
+
+test('renders correctly', () => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires, global-require
+  const { Console } = require('../src/components/Console') as ConsoleComponent;
+  const rendered = render(Console());
+
+  rendered.debug();
+
+  assert.snapshot(
+    rendered.container.innerHTML,
+    `<div id="con">
+<h2>Console Output</h2>
+
+<div class="console code-block code"></div></div>`,
+  );
+});
+
+test.run();
