aca57828aa9469fbdf5db3bfe96d8c2d6a8a7ab8
Max Milton <max@wearegenki.com>
2021-05-15 07:16:16 +1000

    Update dep and fix lint error

--- xcss.config.js
+++ xcss.config.js
@@ -1,3 +1,3 @@
 // @ts-expect-error - No types included yet
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-module.exports = require('@ekscss/framework/xcss.config.js');
+module.exports = require('@ekscss/framework/xcss.config');
