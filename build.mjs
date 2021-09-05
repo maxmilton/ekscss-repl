f1433eb578917531e445e85ca0ba10a3805bf5d3
Max Milton <max@wearegenki.com>
2021-09-05 12:40:46 +1000

    Change ping URL in non-production builds

--- build.mjs
+++ build.mjs
@@ -71,7 +71,11 @@ function makeHtml(jsPath, cssPath) {
 }</script>
 <script src=/${jsPath} defer></script>
 <noscript>You need to enable JavaScript to run this app.</noscript>
-<img src=https://api.trackx.app/v1/8c6cfd78d7e/ping crossorigin>`;
+<img src=${
+  process.env.NODE_ENV === 'production'
+    ? 'https://api.trackx.app/v1/8c6cfd78d7e/ping'
+    : '#'
+} crossorigin>`;
 }
 
 /**
