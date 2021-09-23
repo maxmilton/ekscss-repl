0d74bf15760a27a46090dda3140cf6986b4d471e
Max Milton <max@wearegenki.com>
2021-09-23 17:33:37 +1000

    New input checkbox markdown

--- src/components/Nav.ts
+++ src/components/Nav.ts
@@ -14,13 +14,14 @@ const view = h`
   <div id=nav class="dfc pv1 ph3">
     <h1 id=logo class=mv0>ekscss REPL</h1>
 
-    <a href=https://maxmilton.github.io/ekscss class=pl4 target=_blank rel=noreferrer>Open ekscss docs</a>
+    <a href=https://maxmilton.github.io/ekscss class=pl4 target=_blank rel=noreferrer>ekscss docs</a>
 
-    <label for=auto class="auto label ml-auto mb0 mr3">
-      <input #auto id=auto type=checkbox class="checkbox mr2" checked>
-      Auto compile on input
-    </label>
-    <button #compile class="button button-primary mr3">Compile</button>
+    <div class="df ml-auto">
+      <input #auto id=auto type=checkbox class=checkbox checked>
+      <label for=auto class=label>Auto compile on input</label>
+    </div>
+
+    <button #compile class="button button-primary mh3">Compile</button>
     <button #clear class=button>Clear Output</button>
   </div>
 `;
