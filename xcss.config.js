7d0fe7a38a61cfddb545f9331213a28ecdc0dce9
Max Milton <max@happycode.ltd>
2021-12-07 12:26:20 +1000

    Clean up

--- xcss.config.js
+++ xcss.config.js
@@ -1,13 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-// @ts-expect-error - no types yet
 const { preloadApply } = require('@ekscss/framework/utils');
 const { onBeforeBuild } = require('ekscss');
 
-// generate references so #apply can be used in any file
+// Generate references so #apply can be used in any file
 onBeforeBuild(preloadApply);
 
-// @ts-expect-error - no types yet
 module.exports = require('@ekscss/framework/config');
