fe666a51c8f58dfe9ab615dacd323bd218df277c
Max Milton <max@happyhackers.xyz>
2023-09-12 09:59:53 +1000

    bug: Fix compiled HTML template whitespace next to tags (#694)

--- src/components/Footer.ts
+++ src/components/Footer.ts
@@ -1,6 +1,6 @@
 import { compile } from 'stage1/macro' assert { type: 'macro' };
 import { h } from 'stage1/runtime';
-import { interpolate } from '../macros' assert { type: 'macro' };
+import { interpolate, removeNbsp } from '../macros' assert { type: 'macro' };
 
 export type FooterComponent = HTMLElement;
 
@@ -14,16 +14,17 @@ const meta = compile(
   // FIXME: This is a convoluted workaround for a bug in the bun macro system,
   // where it crashes when doing string literal template interpolation.
   // https://github.com/oven-sh/bun/issues/3641
+
   interpolate(
     `
       <footer id=foot class="mv3 fss muted tc">
-        © <a href=https://maxmilton.com class="normal muted" rel=noreferrer>Max Milton</a> ・ <a href=https://github.com/maxmilton/ekscss rel=noreferrer>ekscss</a> v%%1%% ・ <a href=https://github.com/maxmilton/ekscss-repl rel=noreferrer>REPL</a> %%2%% ・ <a href=https://github.com/maxmilton/ekscss-repl/issues rel=noreferrer>report bug</a>
+        ©&nbsp;<a href=https://maxmilton.com class="normal muted" rel=noreferrer>Max Milton</a>&nbsp;・&nbsp;<a href=https://github.com/maxmilton/ekscss rel=noreferrer>ekscss</a>&nbsp;v%%1%% ・&nbsp;<a href=https://github.com/maxmilton/ekscss-repl rel=noreferrer>REPL</a> %%2%% ・&nbsp;<a href=https://github.com/maxmilton/ekscss-repl/issues rel=noreferrer>report bug</a>
       </footer>
     `,
     [process.env.EKSCSS_VERSION!, process.env.APP_RELEASE!],
   ),
 );
-const view = h<FooterComponent>(meta.html);
+const view = h<FooterComponent>(removeNbsp(meta.html));
 
 export function Footer(): FooterComponent {
   const root = view;
