0fe65fa17eebb61e7c3ed5061069ceb8d09ba7c3
Max Milton <max@happyhackers.xyz>
2024-10-31 13:03:53 +0900

    chore: Rework ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,75 +1,41 @@
-import { fixupPluginRules } from '@eslint/compat';
-import { FlatCompat } from '@eslint/eslintrc';
 import eslint from '@eslint/js';
+import mm from '@maxmilton/eslint-config';
 import security from 'eslint-plugin-security';
 import unicorn from 'eslint-plugin-unicorn';
-// eslint-disable-next-line import/no-unresolved
-import tseslint from 'typescript-eslint';
-
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
+import ts from 'typescript-eslint';
 
 const OFF = 0;
-const WARN = 1;
 const ERROR = 2;
 
-export default tseslint.config(
+export default ts.config(
   eslint.configs.recommended,
-  ...compat.extends('airbnb-base').map((config) => ({
-    ...config,
-    plugins: {}, // delete
-  })),
-  ...compat.extends('airbnb-typescript/base'),
-  ...tseslint.configs.strictTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
-  // @ts-expect-error - no types
-  // eslint-disable-next-line
+  ...ts.configs.strictTypeChecked,
+  ...ts.configs.stylisticTypeChecked,
   unicorn.configs['flat/recommended'],
+  mm.configs.recommended,
   security.configs.recommended,
   {
     linterOptions: {
-      reportUnusedDisableDirectives: WARN,
+      reportUnusedDisableDirectives: ERROR,
     },
     languageOptions: {
       parserOptions: {
         project: ['tsconfig.json', 'tsconfig.node.json'],
+        projectService: {
+          allowDefaultProject: ['*.mjs'],
+        },
         tsconfigRootDir: import.meta.dirname,
       },
     },
-    plugins: {
-      import: fixupPluginRules(
-        compat.plugins('eslint-plugin-import')[0].plugins?.import ?? {},
-      ),
-    },
     rules: {
-      '@typescript-eslint/explicit-module-boundary-types': ERROR,
-      '@typescript-eslint/no-non-null-assertion': WARN,
-      'import/prefer-default-export': OFF,
-      'no-restricted-syntax': OFF,
-      'no-void': OFF,
-      'unicorn/filename-case': OFF,
-      'unicorn/import-style': WARN,
-      'unicorn/no-abusive-eslint-disable': WARN,
-      'unicorn/no-null': OFF,
-      'unicorn/prefer-module': WARN,
-      'unicorn/prefer-top-level-await': WARN,
-      'unicorn/prevent-abbreviations': OFF,
-
-      /* Covered by biome formatter */
-      '@typescript-eslint/indent': OFF,
-      'function-paren-newline': OFF,
-      'implicit-arrow-linebreak': OFF,
-      'max-len': OFF,
-      'object-curly-newline': OFF,
-      'operator-linebreak': OFF,
-      'unicorn/no-nested-ternary': OFF,
+      // bad browser support
+      'unicorn/prefer-at': OFF,
+      // prefer to clearly separate Bun and DOM
+      'unicorn/prefer-global-this': OFF,
 
       /* Performance and byte savings */
       // byte savings
       'no-plusplus': OFF,
-      // poor browser compatibility
-      'unicorn/prefer-string-replace-all': OFF,
       // byte savings (minification doesn't currently automatically remove)
       'unicorn/switch-case-braces': [ERROR, 'avoid'],
 
@@ -82,20 +48,6 @@ export default tseslint.config(
     },
   },
   {
-    files: [
-      '*.config.mjs',
-      '*.config.ts',
-      '*.d.ts',
-      '**/*.spec.ts',
-      '**/*.test.ts',
-      'build.ts',
-      'test/**',
-    ],
-    rules: {
-      'import/no-extraneous-dependencies': OFF,
-    },
-  },
-  {
-    ignores: ['*.bak', 'dist/**'],
+    ignores: ['*.bak', 'coverage/**', 'dist/**'],
   },
 );
