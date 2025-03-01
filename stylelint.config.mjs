f81baed11a2dccdc1cda2aea404fbac96d638063
Max Milton <max@happyhackers.xyz>
2025-03-02 08:02:37 +0900

    chore: Add comment about lint rule

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -5,7 +5,7 @@ export default {
   extends: ['stylelint-config-standard', '@maxmilton/stylelint-config'],
   ignoreFiles: ['*.bak/**', 'dist/**', 'node_modules/**'],
   rules: {
-    'declaration-property-value-no-unknown': null,
+    'declaration-property-value-no-unknown': null, // TODO: Enable this rule + implement in postcss-ekscss
     'function-no-unknown': null,
     'import-notation': null,
     'media-query-no-invalid': null,
