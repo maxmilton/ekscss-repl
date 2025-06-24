f3125fa9aef16214a916e119799e04dbad5d288a
Max Milton <max@happyhackers.xyz>
2025-06-24 18:59:01 +0900

    feat: Refactor build

--- build.ts
+++ build.ts
@@ -1,31 +1,21 @@
 /* eslint-disable no-await-in-loop, no-bitwise, no-console */
 
 import { basename } from 'node:path'; // eslint-disable-line unicorn/import-style
+import * as swc from '@swc/core';
 import type { BuildArtifact, BunPlugin } from 'bun';
-import * as csso from 'csso';
-import type { Usage } from 'csso';
-import * as xcss from 'ekscss';
 import type { XCSSCompileOptions } from 'ekscss';
+import * as xcss from 'ekscss';
 import * as lightningcss from 'lightningcss';
-import type { Warning } from 'lightningcss';
 import { PurgeCSS, type RawContent } from 'purgecss';
-import * as terser from 'terser';
-import type { MinifyOptions } from 'terser';
-import pkg from './package.json' assert { type: 'json' };
-import xcssConfig from './xcss.config';
+import pkg from './package.json' with { type: 'json' };
+import xcssConfig from './xcss.config.ts';
+
+const gitHash = Bun.spawnSync(['git', 'rev-parse', '--short', 'HEAD']).stdout.toString().trim();
+const isDirty = Bun.spawnSync(['git', 'diff', '--quiet']).exitCode !== 0;
 
 const mode = Bun.env.NODE_ENV;
 const dev = mode === 'development';
-
-const release = Bun.spawnSync([
-  'git',
-  'describe',
-  '--always',
-  '--dirty=-dev',
-  '--broken',
-])
-  .stdout.toString()
-  .trim();
+const release = `v${pkg.version}-${gitHash}${isDirty ? '-dev' : ''}`;
 
 export function xcssPlugin(config: XCSSCompileOptions): BunPlugin {
   return {
@@ -38,175 +28,151 @@ export function xcssPlugin(config: XCSSCompileOptions): BunPlugin {
           globals: config.globals,
           plugins: config.plugins,
         });
-
         for (const warning of compiled.warnings) {
-          console.error('[XCSS]', warning.message);
-
+          console.error('XCSS:', warning.message);
           if (warning.file) {
             console.log(
-              `  at ${[warning.file, warning.line, warning.column]
-                .filter(Boolean)
-                .join(':')}`,
+              `  at ${[warning.file, warning.line, warning.column].filter(Boolean).join(':')}`,
             );
           }
         }
-
         return { contents: compiled.css, loader: 'css' };
       });
     },
   };
 }
 
-/** Print lightningcss compile warnings. */
-export function printWarnings(warnings: Warning[]): void {
-  for (const warning of warnings) {
-    console.error(`[LightningCSS] ${warning.type}:`, warning.message);
-    console.log(
-      `  at ${warning.loc.filename}:${String(warning.loc.line)}:${String(warning.loc.column)}`,
-    );
-    if (warning.value) {
-      console.log(warning.value);
-    }
-  }
-}
+async function minify(artifacts: BuildArtifact[]): Promise<void> {
+  const artifactsJs: BuildArtifact[] = [];
+  const artifactsCss: BuildArtifact[] = [];
 
-export async function minifyCSS(
-  artifacts: BuildArtifact[],
-  htmlCode: string,
-  safelist: string[] = ['html', 'body'],
-  blocklist: Usage['blacklist'] = {},
-): Promise<void> {
-  const content: RawContent[] = [{ extension: '.html', raw: htmlCode }];
-  const purgecss = new PurgeCSS();
   const encoder = new TextEncoder();
+  const purgecss = new PurgeCSS();
+  const content: RawContent[] = [];
 
   for (const artifact of artifacts) {
-    if (artifact.kind === 'entry-point' || artifact.kind === 'chunk') {
-      content.push({ extension: '.js', raw: await artifact.text() });
+    if (artifact.path.endsWith('.js')) {
+      artifactsJs.push(artifact);
+    } else if (artifact.path.endsWith('.css')) {
+      artifactsCss.push(artifact);
     }
   }
 
-  for (const artifact of artifacts) {
-    if (artifact.kind === 'asset' && artifact.path.endsWith('.css')) {
-      const filename = basename(artifact.path);
-      const source = await artifact.text();
-      const purged = await purgecss.purge({
-        content,
-        css: [
-          {
-            raw: source
-              // HACK: Workaround for JS style sourcemap comments rather than CSS.
-              //  ↳ This is a bug in bun: https://github.com/oven-sh/bun/issues/15532
-              .replace(/\/\/# debugId=[\w]+\n/, '')
-              .replace(
-                /\/\/# sourceMappingURL=([-\w.]+\.css\.map)\n?$/,
-                '/*# sourceMappingURL=$1 */',
-              ),
-          },
-        ],
-        safelist,
-      });
-      const minified = lightningcss.transform({
-        filename,
-        code: encoder.encode(purged[0].css),
-        minify: true,
-        targets: {
-          chrome: 60 << 16, // FIXME: Set to >= 123 ?... Actually seems fine with no junk
-          edge: 79 << 16,
-          firefox: 55 << 16,
-          safari: (11 << 16) | (1 << 8),
-        },
-      });
-      printWarnings(minified.warnings);
-      const minified2 = csso.minify(minified.code.toString(), {
-        filename,
-        forceMediaMerge: true, // somewhat unsafe
-        usage: {
-          blacklist: blocklist,
-        },
-        // debug: true,
-      });
-
-      await Bun.write(artifact.path, minified2.css);
-    }
-  }
-}
-
-export async function minifyJS(
-  artifacts: BuildArtifact[],
-  options?: Omit<MinifyOptions, 'sourceMap'>,
-): Promise<void> {
-  for (const artifact of artifacts) {
+  for (const artifact of artifactsJs) {
     if (artifact.kind === 'entry-point' || artifact.kind === 'chunk') {
       const source = await artifact.text();
-      const sourceMap = artifact.sourcemap;
-      const result = await terser.minify(source, {
+      const result = await swc.minify(source, {
         ecma: 2020,
-        module: true,
-        format: {
-          wrap_func_args: true,
-          wrap_iife: true,
-        },
+        // module: true,
         compress: {
           comparisons: false,
           negate_iife: false,
           reduce_funcs: false,
+          passes: 2,
+
+          // XXX: Comment out to keep performance markers for debugging.
+          pure_funcs: ['performance.mark', 'performance.measure'],
+        },
+        format: {
+          wrap_func_args: true,
+          wrap_iife: true,
         },
         mangle: {
-          properties: {
-            regex: /^\$\$/,
+          props: {
+            regex: String.raw`^\$\$`,
           },
         },
-        ...options,
-        sourceMap: sourceMap
-          ? {
-              content: await sourceMap.text(),
-              filename: basename(artifact.path),
-              url: basename(sourceMap.path),
-            }
-          : false,
+        sourceMap: Boolean(artifact.sourcemap),
       });
 
-      if (result.code) {
-        await Bun.write(artifact.path, result.code);
+      await Bun.write(artifact.path, result.code);
+      if (artifact.sourcemap && result.map) {
+        await Bun.write(artifact.sourcemap.path, result.map);
       }
-      if (sourceMap && result.map) {
-        // eslint-disable-next-line @typescript-eslint/no-base-to-string
-        await Bun.write(sourceMap.path, result.map.toString());
-      }
+      content.push({ extension: '.js', raw: result.code });
+    }
+  }
+
+  for (const artifact of artifactsCss) {
+    const filename = basename(artifact.path);
+    const source = await artifact.text();
+    const [purged] = await purgecss.purge({
+      content,
+      css: [{ raw: source }],
+      safelist: ['html', 'body'],
+      blocklist: ['object'],
+      sourceMap: Boolean(artifact.sourcemap),
+    });
+    const minified = lightningcss.transform({
+      filename,
+      code: encoder.encode(purged.css),
+      minify: true,
+      targets: {
+        android: 60 << 16,
+        chrome: 60 << 16,
+        edge: 79 << 16,
+        firefox: 55 << 16,
+        ios_saf: (11 << 16) | (1 << 8),
+        safari: (11 << 16) | (1 << 8),
+      },
+      include: lightningcss.Features.Nesting | lightningcss.Features.MediaQueries,
+      exclude: lightningcss.Features.LogicalProperties | lightningcss.Features.LightDark,
+      sourceMap: Boolean(artifact.sourcemap),
+      inputSourceMap: purged.sourceMap!,
+    });
+    if (minified.warnings.length > 0) console.error(minified.warnings);
+    await Bun.write(artifact.path, minified.code);
+    if (artifact.sourcemap && minified.map) {
+      await Bun.write(artifact.sourcemap.path, minified.map);
     }
   }
 }
 
 async function buildHTML(artifacts: BuildArtifact[]) {
-  const entrypoint = artifacts.find((a) => a.kind === 'entry-point');
-  const css = artifacts.find(
-    (a) => a.kind === 'asset' && a.path.endsWith('.css'),
-  );
-  if (!entrypoint) throw new Error('Could not find entry point JS artifact');
+  const js = artifacts.find((a) => a.kind === 'entry-point' && a.path.endsWith('.js'));
+  const css = artifacts.find((a) => a.kind === 'asset' && a.path.endsWith('.css'));
+  if (!js) throw new Error('Could not find JS artifact');
   if (!css) throw new Error('Could not find CSS artifact');
+
+  const cssFile = basename(css.path);
+  const jsFile = basename(js.path);
 
   const html = `
     <!doctype html>
+    <html lang=en>
     <meta charset=utf-8>
     <meta name=viewport content="width=device-width">
-    <meta name=google value=notranslate>
     <meta name=theme-color content=#f5f8fa>
-    <link href=/app.webmanifest rel=manifest>
+    <link href=/manifest.webmanifest rel=manifest>
     <link href=/favicon.svg rel=icon>
     <link href=/apple-touch-icon.png rel=apple-touch-icon>
     <title>ekscss REPL</title>
-    <link href=/${basename(css.path)} rel=stylesheet>
-    <script src=https://cdn.jsdelivr.net/npm/trackx@0/default.js crossorigin></script>
-    <script>window.trackx&&(trackx.setup("https://api.trackx.app/v1/8c6cfd78d7e"),trackx.ping());</script>
-    <script src=/${basename(entrypoint.path)} defer></script>
+    <link href=/${cssFile} rel=stylesheet>
+    <script src=https://io.bugbox.app/v0/bugbox.js crossorigin data-key=AZdUwYn8cACA8WMnLa8QKQ data-release=${release} data-env=${String(process.env.NODE_ENV)} data-ekscss=${pkg.dependencies.ekscss}></script>
+    <script src=/${basename(jsFile)} defer></script>
     <noscript>You need to enable JavaScript to run this app.</noscript>
   `
     .trim()
     .replace(/\n\s+/g, '\n'); // remove leading whitespace
-
+  const html404 = `
+    <!doctype html>
+    <html lang=en>
+    <meta charset=utf-8>
+    <meta name=viewport content="width=device-width">
+    <meta name=theme-color content=#f5f8fa>
+    <link href=/favicon.svg rel=icon>
+    <title>404 Not Found</title>
+    <link href=/${cssFile} rel=stylesheet>
+    <body class=ph3>
+      <h1>404 Not Found</h1>
+      <p>The resource you are looking for does not exist.</p>
+  `
+    .trim()
+    .replace(/\n\s+/g, '\n'); // remove leading whitespace
   await Bun.write('dist/index.html', html);
-  return html;
+  await Bun.write('dist/404.html', html404);
+
+  return { cssFile, jsFile };
 }
 
 console.time('prebuild');
@@ -220,7 +186,7 @@ const out = await Bun.build({
   outdir: 'dist',
   naming: dev ? '[dir]/[name].[ext]' : '[dir]/[name]-[hash].[ext]',
   target: 'browser',
-  format: 'iife',
+  // format: 'iife',
   define: {
     'process.env.APP_RELEASE': JSON.stringify(release),
     'process.env.EKSCSS_VERSION': JSON.stringify(pkg.dependencies.ekscss),
@@ -230,7 +196,7 @@ const out = await Bun.build({
     '.svg': 'text',
   },
   plugins: [xcssPlugin(xcssConfig)],
-  emitDCEAnnotations: true, // for terser
+  emitDCEAnnotations: true,
   minify: !dev,
   sourcemap: 'linked',
 });
@@ -239,25 +205,25 @@ console.log(out);
 if (!out.success) throw new AggregateError(out.logs, 'Build failed');
 
 console.time('html');
-const html = await buildHTML(out.outputs);
+const out2 = await buildHTML(out.outputs);
 console.timeEnd('html');
 
 if (!dev) {
-  console.time('minify:css');
-  await minifyCSS(out.outputs, html);
-  console.timeEnd('minify:css');
-
-  console.time('minify:js');
-  await minifyJS(out.outputs, {
-    module: false,
-    compress: {
-      comparisons: false,
-      negate_iife: false,
-      reduce_funcs: false, // prevent function inlining
-      passes: 3,
-      // XXX: Comment out to keep performance markers for debugging.
-      pure_funcs: ['performance.mark', 'performance.measure'],
-    },
-  });
-  console.timeEnd('minify:js');
+  console.time('minify');
+  await minify(out.outputs);
+  console.timeEnd('minify');
 }
+
+console.time('build-info');
+await Bun.write(
+  'dist/build-info.json',
+  JSON.stringify({
+    ref: Bun.spawnSync(['git', 'describe', '--always', '--dirty=-dev', '--broken'])
+      .stdout.toString()
+      .trim(),
+    mode,
+    css: out2.cssFile,
+    js: out2.jsFile,
+  }),
+);
+console.timeEnd('build-info');
