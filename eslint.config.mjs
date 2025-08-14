dfeee73aaff8946bd8328de7aa3ba20395cea416
Max Milton <max@happyhackers.xyz>
2025-08-14 19:49:19 +0900

    chore: Refactor eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,15 @@
-import eslint from '@eslint/js';
-import mm from '@maxmilton/eslint-config';
-import unicorn from 'eslint-plugin-unicorn';
-import ts from 'typescript-eslint';
+import js from "@eslint/js";
+import mm from "@maxmilton/eslint-config";
+import unicorn from "eslint-plugin-unicorn";
+import ts from "typescript-eslint";
 
 const OFF = 0;
 const ERROR = 2;
 
 export default ts.config(
-  eslint.configs.recommended,
-  ...ts.configs.strictTypeChecked,
-  ...ts.configs.stylisticTypeChecked,
+  js.configs.recommended,
+  ts.configs.strictTypeChecked,
+  ts.configs.stylisticTypeChecked,
   unicorn.configs.recommended,
   mm.configs.recommended,
   {
@@ -18,33 +18,20 @@ export default ts.config(
     },
     languageOptions: {
       parserOptions: {
-        projectService: {
-          allowDefaultProject: ['*.mjs'],
-        },
+        projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
     rules: {
-      // bad browser support
-      'unicorn/prefer-at': OFF,
-      // prefer to clearly separate Bun and DOM
-      'unicorn/prefer-global-this': OFF,
-
-      /* Performance and byte savings */
-      // byte savings
-      'no-plusplus': OFF,
-      // byte savings (minification doesn't currently automatically remove)
-      'unicorn/switch-case-braces': [ERROR, 'avoid'],
-
-      /* stage1 */
-      // underscores in synthetic event handler names
-      'no-underscore-dangle': OFF,
-      'unicorn/prefer-add-event-listener': OFF,
-      'unicorn/prefer-dom-node-append': OFF,
-      'unicorn/prefer-query-selector': OFF,
+      "no-plusplus": OFF, // byte savings
+      quotes: [ERROR, "double", { avoidEscape: true }],
+      "unicorn/prefer-add-event-listener": OFF,
+      "unicorn/prefer-at": OFF, // bad browser support
+      "unicorn/prefer-dom-node-append": OFF,
+      "unicorn/prefer-global-this": OFF, // prefer to clearly separate Bun and DOM
+      "unicorn/prefer-query-selector": OFF,
+      "unicorn/switch-case-braces": [ERROR, "avoid"], // byte savings
     },
   },
-  {
-    ignores: ['coverage', 'dist'],
-  },
+  { ignores: ["coverage", "dist"] },
 );
